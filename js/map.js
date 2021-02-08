54aa90c1df6412cd005297cd08719f0e154e2590
Bec <70154628+BecWy@users.noreply.github.com>
2021-02-08 12:31:18 +0000

    Updated contact details

--- js/map.js
+++ js/map.js
@@ -1,8 +1,13 @@
-var mymap = L.map('mapid').setView([52.0527, 1.161], 13);
-
 //Ipswich waterfront coordinates: [52.0527, 1.161]
 
-var marker = L.marker([52.0527, 1.161]).addTo(mymap);
+
+//Norwich coordinates
+//52.6476° N, 1.2638° E
+var mymap = L.map('mapid').setView([52.6476, 1.2638], 13);
+
+
+
+var marker = L.marker([52.6476, 1.2638]).addTo(mymap);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
